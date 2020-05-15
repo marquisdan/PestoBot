07f58bd5f558fc483fc281bbe5b4a0f8be187703
Dan Hunt <Daniel.Hunt@sumtotalsystems.com>
2020-05-15 16:24:38 -0500

    Added some common text formatting to its own class

---
+++ Common/TextUtils.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace PestoBot.Common
+{
+    public static class TextUtils
+    {
+        public static string GetSuccessText(string msg)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("```diff");
+            sb.AppendLine($"+ {msg}");
+            sb.AppendLine("```");
+            return sb.ToString();
+        }
+
+        public static string GetWarnText(string msg)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("```fix");
+            sb.AppendLine($"{msg}");
+            sb.AppendLine("```");
+            return sb.ToString();
+        }
+
+        public static string GetErrorText(string msg)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("```diff");
+            sb.AppendLine($"- {msg}");
+            sb.AppendLine("```");
+            return sb.ToString();
+        }
+    }
+}
