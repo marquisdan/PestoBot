841dfcc5fe4e52b42873fe8c3d28646a95e5093a
Dan Hunt <Daniel.Hunt@sumtotalsystems.com>
2020-07-09 15:42:04 -0500

    Renamed Debug Person model file to match DB table name

---
+++ Database/Models/Debug/DebugPersonModel.cs
@@ -0,0 +1,11 @@
+﻿using PestoBot.Database.Models.Common;
+
+namespace PestoBot.Database.Models.DebugModel
+{
+    public class DebugPersonModel : AbstractPestoModel
+    {
+        //Simple model for testing DB 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
